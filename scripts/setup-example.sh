88c0bd042715b3905fc0abf30851158548618d37
FabijanC <fabijan.corak@gmail.com>
2021-12-15 13:48:17 +0100

    Install plugin from source

--- scripts/setup-example.sh
+++ scripts/setup-example.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "Cloning branch 'devnet'"
+echo "Installing starknet-hardhat-plugin from source"
+git clone -b master -single-branch git@github.com:Shard-Labs/starknet-hardhat-plugin.git
+cd starknet-hardhat-example
+npm ci
+npm run build
+npm link
+
+echo "Cloning starknet-hardhat-example branch 'devnet'"
 git clone -b devnet --single-branch git@github.com:Shard-Labs/starknet-hardhat-example.git
 cd starknet-hardhat-example
 npm ci
+npm link @shardlabs/starknet-hardhat-plugin
 
 # generate artifacts
 npx hardhat starknet-compile
