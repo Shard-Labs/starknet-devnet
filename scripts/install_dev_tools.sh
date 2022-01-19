2186300913d7ad2f19781b68fd7a8c2562bbe577
FabijanC <fabijan.corak@gmail.com>
2022-01-19 13:12:48 +0100

    Set CAIRO_LANG_VERSION as first CircleCI step

--- scripts/install_dev_tools.sh
+++ scripts/install_dev_tools.sh
@@ -2,5 +2,4 @@
 
 set -e
 
-CAIRO_LANG_VERSION=$(./scripts/get_version.sh cairo-lang)
 pip3 install poetry "cairo-lang==$CAIRO_LANG_VERSION"
--- test/hardhat.config.dockerized.ts
+++ test/hardhat.config.dockerized.ts
@@ -1,8 +1,13 @@
 import "@shardlabs/starknet-hardhat-plugin";
 
+if (!process.env.CAIRO_LANG_VERSION) {
+    console.log("CAIRO_LANG_VERSION is undefined.")
+    process.exit(1);
+}
+
 module.exports = {
     cairo: {
-        version: "latest"
+        version: process.env.CAIRO_LANG_VERSION
     },
     networks: {
         devnet: {
