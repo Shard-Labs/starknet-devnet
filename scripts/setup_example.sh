7330ced55cf10a43b21c07abb0c3af7773839dd2
FabijanC <fabijan.corak@gmail.com>
2022-03-16 11:13:45 +0100

    Use example repo branch axios-version

--- scripts/setup_example.sh
+++ scripts/setup_example.sh
@@ -10,12 +10,11 @@ npm link
 cd ..
 
 echo "Cloning starknet-hardhat-example branch 'devnet'"
-git clone -b devnet --single-branch git@github.com:Shard-Labs/starknet-hardhat-example.git
+git clone -b axios-version --single-branch git@github.com:Shard-Labs/starknet-hardhat-example.git
 cd starknet-hardhat-example
 npm ci
 npm link @shardlabs/starknet-hardhat-plugin
 
 # generate artifacts
 npx hardhat starknet-compile
-
 npx hardhat compile
