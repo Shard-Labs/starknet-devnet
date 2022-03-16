a0b9de4532da77023949dce90c883794a37a11e9
FabijanC <fabijan.corak@gmail.com>
2022-03-16 14:57:08 +0100

    Use pyenv

--- scripts/install_dev_tools.sh
+++ scripts/install_dev_tools.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+echo "pyenv: $(pyenv --version)"
 echo "npm: $(npm --version)"
 echo "node: $(node --version)"
 echo "pip: $(pip --version)"
@@ -9,5 +10,11 @@ echo "pip3: $(pip3 --version)"
 echo "python: $(python --version)"
 echo "python3: $(python3 --version)"
 
+echo "pyenv versions"
+pyenv versions
+pyenv global 3.7
+
 CAIRO_LANG_VERSION=$(./scripts/get_version.sh cairo-lang)
 pip3 install poetry "cairo-lang==$CAIRO_LANG_VERSION"
+
+poetry env use $(which python)
