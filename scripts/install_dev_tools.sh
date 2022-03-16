532c13809d65e32388ff8ea2ad74213ff44a23f1
FabijanC <fabijan.corak@gmail.com>
2022-03-16 15:09:30 +0100

    Log available python installations

--- scripts/install_dev_tools.sh
+++ scripts/install_dev_tools.sh
@@ -10,10 +10,13 @@ echo "pip3: $(pip3 --version)"
 echo "python: $(python --version)"
 echo "python3: $(python3 --version)"
 
-pyenv install 3.8.12
-echo "pyenv versions"
+echo "pyenv available versions:"
+pyenv install --list
+
+PY_VERSION=3.8.12
+pyenv install "$PY_VERSION"
 pyenv versions
-pyenv global 3.8.12
+pyenv global "$PY_VERSION"
 
 CAIRO_LANG_VERSION=$(./scripts/get_version.sh cairo-lang)
 pip3 install poetry "cairo-lang==$CAIRO_LANG_VERSION"
