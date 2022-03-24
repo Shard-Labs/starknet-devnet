d4b67126c16a581bfa02d29930b89f6cfa7fd6f5
Antun Badurina <antun.badurina@gmail.com>
2022-03-24 10:55:07 +0100

    Add testing placeholder

---
+++ test/test_restart.py
@@ -0,0 +1,37 @@
+"""
+Test restart endpoint
+"""
+
+import pytest
+import requests
+
+from .settings import APP_URL
+from .util import run_devnet_in_background
+
+@pytest.fixture(autouse=True)
+def run_before_and_after_test():
+    """Run devnet before and kill it after the test run"""
+    # before test
+    devnet_proc = run_devnet_in_background()
+
+    yield
+
+    # after test
+    devnet_proc.kill()
+
+def get_restart_response():
+    """Get restart response"""
+    print(f"{APP_URL}/restart")
+    return requests.get(f"{APP_URL}/restart")
+
+@pytest.mark.restart
+def test_restart_on_initial_state():
+    """Checks restart endpoint when there were no changes"""
+    res = get_restart_response()
+    assert res.status_code == 200
+
+@pytest.mark.restart
+def test_restart_deployed_contracts():
+    """Checks restart endpoint when there were no changes"""
+    res = get_restart_response()
+    assert res.status_code == 200
