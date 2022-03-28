666b3b95b40da705b0b9157c217457cb28092155
Antun Badurina <antun.badurina@gmail.com>
2022-03-28 17:01:12 +0200

    Add double underscore [skip ci]

--- starknet_devnet/state.py
+++ starknet_devnet/state.py
@@ -13,17 +13,17 @@ class State():
         self.starknet_wrapper = StarknetWrapper()
         self.dumper = Dumper(self.starknet_wrapper)
 
-    def _set_starknet_wrapper(self, starknet_wrapper: StarknetWrapper):
+    def __set_starknet_wrapper(self, starknet_wrapper: StarknetWrapper):
         """Sets starknet wrapper and creates new instance of dumper"""
         self.starknet_wrapper = starknet_wrapper
         self.dumper = Dumper(starknet_wrapper)
 
     def reset(self):
         """Reset the starknet wrapper and dumper instances"""
-        self._set_starknet_wrapper(StarknetWrapper())
+        self.__set_starknet_wrapper(StarknetWrapper())
 
     def load(self, load_path: str):
         """Loads starknet wrapper from path"""
-        self._set_starknet_wrapper(StarknetWrapper.load(load_path))
+        self.__set_starknet_wrapper(StarknetWrapper.load(load_path))
 
 state = State()
