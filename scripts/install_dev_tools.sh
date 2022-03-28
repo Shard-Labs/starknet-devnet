89d914249fbe9e7edafe561e9f4517e693ca8206
Diogo Ribeiro <dr@shardlabs.io>
2022-03-28 14:21:03 +0100

    Test paralelism

--- scripts/install_dev_tools.sh
+++ scripts/install_dev_tools.sh
@@ -14,8 +14,14 @@ echo "pyenv available versions:"
 pyenv install --list
 
 PY_VERSION=3.8.9
-pyenv install "$PY_VERSION"
-pyenv versions
+
+pyenv versions > installed_pyenv_versions.txt
+echo "$(grep -q $PY_VERSION installed_pyenv_versions.txt )"
+if ! grep -q $PY_VERSION installed_pyenv_versions.txt 
+then
+    pyenv install "$PY_VERSION"
+fi
+
 pyenv global "$PY_VERSION"
 echo "Using global python at: $(which python)"
 
