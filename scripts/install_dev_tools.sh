d1a735552f3d12e2f839a778b639290f8676a83c
Diogo Ribeiro <dr@shardlabs.io>
2022-03-29 14:06:58 +0100

    [skip ci] fixed PR

--- scripts/install_dev_tools.sh
+++ scripts/install_dev_tools.sh
@@ -14,14 +14,8 @@ echo "pyenv available versions:"
 pyenv install --list
 
 PY_VERSION=3.8.9
-
-pyenv versions > installed_pyenv_versions.txt
-echo "$(grep -q $PY_VERSION installed_pyenv_versions.txt )"
-if ! grep -q $PY_VERSION installed_pyenv_versions.txt 
-then
-    pyenv install "$PY_VERSION"
-fi
-
+pyenv install "$PY_VERSION"
+pyenv versions
 pyenv global "$PY_VERSION"
 echo "Using global python at: $(which python)"
 
