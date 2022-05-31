6c452ac75bce0e8ec719c760a037e2806b3f3550
Antun Badurina <antun.badurina@gmail.com>
2022-05-31 14:25:58 +0200

    Use assert equal

--- test/test_state_update.py
+++ test/test_state_update.py
@@ -8,7 +8,9 @@ import requests
 from starkware.starknet.core.os.contract_hash import compute_contract_hash
 from starkware.starknet.public.abi import get_selector_from_name
 
-from .util import deploy, invoke, load_contract_definition, devnet_in_background, get_block
+from .util import (
+    deploy, invoke, load_contract_definition, devnet_in_background, get_block, assert_equal
+)
 from .settings import FEEDER_GATEWAY_URL
 from .shared import STORAGE_CONTRACT_PATH, STORAGE_ABI_PATH
 
@@ -54,7 +56,7 @@ def test_initial_state_update():
     """Test initial state update"""
     state_update = get_state_update()
 
-    assert state_update is None
+    assert_equal(state_update, None)
 
 @pytest.mark.state_update
 @devnet_in_background()
@@ -65,12 +67,12 @@ def test_deployed_contracts():
     state_update = get_state_update()
     deployed_contracts = state_update["state_diff"]["deployed_contracts"]
 
-    assert len(deployed_contracts) == 1
-    assert int(deployed_contracts[0]["address"], 16) == int(contract_address, 16)
+    assert_equal(len(deployed_contracts), 1)
+    assert_equal(int(deployed_contracts[0]["address"], 16), int(contract_address, 16))
 
     deployed_contract_hash =  deployed_contracts[0]["contract_hash"]
 
-    assert int(deployed_contract_hash, 16) == get_contract_hash()
+    assert_equal(int(deployed_contract_hash, 16), get_contract_hash())
 
 @pytest.mark.state_update
 @devnet_in_background()
@@ -82,13 +84,13 @@ def test_storage_diff():
 
     state_update = get_state_update()
     storage_diffs = state_update["state_diff"]["storage_diffs"]
-    assert len(storage_diffs) == 1
+    assert_equal(len(storage_diffs), 1)
 
     contract_storage_diffs = storage_diffs[address]
 
-    assert len(contract_storage_diffs) == 1
-    assert contract_storage_diffs[0]["value"] == hex(30)
-    assert contract_storage_diffs[0]["key"] == STORAGE_KEY
+    assert_equal(len(contract_storage_diffs), 1)
+    assert_equal(contract_storage_diffs[0]["value"], hex(30))
+    assert_equal(contract_storage_diffs[0]["key"], STORAGE_KEY)
 
     invoke("store_value", ["0"], contract_address, STORAGE_ABI_PATH)
 
@@ -96,9 +98,9 @@ def test_storage_diff():
     storage_diffs = state_update["state_diff"]["storage_diffs"]
     contract_storage_diffs = storage_diffs[address]
 
-    assert len(contract_storage_diffs) == 1
-    assert contract_storage_diffs[0]["value"] == hex(0)
-    assert contract_storage_diffs[0]["key"] == STORAGE_KEY
+    assert_equal(len(contract_storage_diffs), 1)
+    assert_equal(contract_storage_diffs[0]["value"], hex(0))
+    assert_equal(contract_storage_diffs[0]["key"], STORAGE_KEY)
 
 @pytest.mark.state_update
 @devnet_in_background()
@@ -110,7 +112,7 @@ def test_block_hash():
     first_block = get_block(parse=True)
     first_block_hash = first_block["block_hash"]
 
-    assert first_block_hash == initial_state_update["block_hash"]
+    assert_equal(first_block_hash, initial_state_update["block_hash"])
 
     # creates new block
     deploy_empty_contract()
@@ -119,7 +121,7 @@ def test_block_hash():
     previous_state_update = get_state_update(first_block_hash)
 
     assert new_state_update["block_hash"] != first_block_hash
-    assert previous_state_update == initial_state_update
+    assert_equal(previous_state_update, initial_state_update)
 
 @pytest.mark.state_update
 @devnet_in_background()
@@ -127,7 +129,7 @@ def test_wrong_block_hash():
     """Test wrong block hash in the state update"""
     state_update_response = get_state_update_response(block_hash="WRONG_HASH")
 
-    assert state_update_response.status_code == 500
+    assert_equal(state_update_response.status_code, 500)
 
 @pytest.mark.state_update
 @devnet_in_background()
@@ -143,8 +145,8 @@ def test_block_number():
     first_block_state_update = get_state_update(block_number=0)
     second_block_state_update = get_state_update(block_number=1)
 
-    assert first_block_state_update == initial_state_update
-    assert second_block_state_update == new_state_update
+    assert_equal(first_block_state_update, initial_state_update)
+    assert_equal(second_block_state_update, new_state_update)
 
 @pytest.mark.state_update
 @devnet_in_background()
@@ -152,7 +154,7 @@ def test_wrong_block_number():
     """Test wrong block hash in the state update"""
     state_update_response = get_state_update_response(block_number=42)
 
-    assert state_update_response.status_code == 500
+    assert_equal(state_update_response.status_code, 500)
 
 @pytest.mark.state_update
 @devnet_in_background()
@@ -172,4 +174,4 @@ def test_roots():
     state_update = get_state_update()
     old_root = state_update["old_root"]
 
-    assert old_root == new_root
+    assert_equal(old_root, new_root)
