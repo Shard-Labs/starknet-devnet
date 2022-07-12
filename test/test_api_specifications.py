a04d5b8af922d713217ed5176cfdf4e429fb279c
FabijanC <fabijan.corak@gmail.com>
2022-07-12 17:58:11 +0200

    Change single with double quotes in api test [skip ci]

--- test/test_api_specifications.py
+++ test/test_api_specifications.py
@@ -9,5 +9,5 @@ def test_api_endpoint():
     """Assert that /api endpoint return list of endpoints"""
     response = app.test_client().get(f"{APP_URL}/api")
     assert response.status_code == 200
-    assert response.json['/api']['functionName'] == 'api'
-    assert response.json['/api']['doc'] == 'Return available endpoints.'
+    assert response.json["/api"]["functionName"] == "api"
+    assert response.json["/api"]["doc"] == "Return available endpoints."
